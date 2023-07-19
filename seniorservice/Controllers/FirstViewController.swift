1d82d2531b46072c3f863befb9c77269bf14adb9
Negin Maka <negin.maka@gmail.com>
2023-07-18 19:37:15 -0700

    Updated the navigation controller

--- seniorservice/Controllers/FirstViewController.swift
+++ seniorservice/Controllers/FirstViewController.swift
@@ -8,26 +8,7 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
-    @IBOutlet weak var homeButton: UIButton!
-    @IBOutlet weak var healthButton: UIButton!
-    @IBOutlet weak var PreviousRequestButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    @IBAction func homeButton(_ sender: Any) {
-        let storyboard = self.storyboard?.instantiateViewController(withIdentifier: "HomeServiceController") as! HomeViewController
-        self.navigationController?.pushViewController(storyboard, animated: true)
-    }
-    
-    @IBAction func healthButton(_ sender: Any) {
-        let storyboard = self.storyboard?.instantiateViewController(withIdentifier: "HealthServiceController") as! HealthViewController
-        self.navigationController?.pushViewController(storyboard, animated: true)
-    }
-    
-    @IBAction func PreviousRequestButton(_ sender: Any) {
-        self.performSegue(withIdentifier: "PreviousRequest", sender: self)
     }
 }
--- seniorservice/Controllers/ServiceRequestViewController.swift
+++ seniorservice/Controllers/ServiceRequestViewController.swift
@@ -15,13 +15,41 @@ class ServiceRequestViewController: UIViewController {
     var serviceName: String!
     var categoryName: String!
     
+    //var description: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         serviceNameLabel.text = serviceName
         categoryNameLabel.text = categoryName
     }
     
-    @IBAction func SubmitButton(_ sender: Any) {
-        self.performSegue(withIdentifier: "Submit", sender: self)
+    @IBAction func submitButton(_ sender: Any) {
+        
+        // TODO: Create a Request object based on user input
+        
+//        var request = Request()
+//            request.description = descriptionTextField.text
+//            request.createdDate = Date()
+//            request.status = .created
+        }
+        
+        // TODO: (Temporary until DB is created) Load all requests and serialize them to an array of Requests
+        
+        // TODO: Append the new Request object to the list of Requests
+        
+        // TODO: Push the new Request list to DB aka the jsonFile
+        
+        // TODO: Create a status check for the push:
+        // TODO: if all successful: show success pop up and navigate to root
+        // Show popup
+        let alertController = UIAlertController(title: "Request Sent", message: "Your request was successfully sent", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Go back to home", style: .default, handler: { _ in
+            // Navigate to root view
+            self.navigationController?.popToRootViewController(animated: true)
+        }))
+        
+        present(alertController, animated: true, completion: nil)
+        
+        // TODO: if failed: show failed pop up and navigate to the same view
+    
     }
-}
