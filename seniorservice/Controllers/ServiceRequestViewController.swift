7b9dd9a82d6c6b4727adde14b585557459bdc7d4
Eduardo Meira <eduardo@xpmind.com>
2023-07-26 19:54:26 -0400

    a few more potential suggestions

--- seniorservice/Controllers/ServiceRequestViewController.swift
+++ seniorservice/Controllers/ServiceRequestViewController.swift
@@ -31,20 +31,35 @@ class ServiceRequestViewController: UIViewController, UITextViewDelegate, UIText
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         let formattedDate = dateFormatter.string(from: currentDate)
-        
+		self.validateBudgetInput()
         // Initialize newRequestToBeCreated here
-        if let budgetText = budgetTextField.text, let budget = Double(budgetText) {
-            newRequestToBeCreated = Request(category: self.categoryName, service: self.serviceName, budget: budget, description: self.descriptionTextView.text, isUrgent: true, createdDate: formattedDate, status: "Not Done")
-        } else {
-            // Set a default budget value if the budgetTextField is empty or invalid
-            newRequestToBeCreated = Request(category: self.categoryName, service: self.serviceName, budget: 0.0, description: self.descriptionTextView.text, isUrgent: true, createdDate: formattedDate, status: "Not Done")
-            budgetTextField.text = "$0.00" // Set the initial text with the $0.00 mask
-        }
+
+//
+//        if let budgetText = budgetTextField.text, let budget = Double(budgetText) {
+//            newRequestToBeCreated = Request(category: self.categoryName, service: self.serviceName, budget: budget, description: self.descriptionTextView.text, isUrgent: true, createdDate: formattedDate, status: "Not Done")
+//        } else {
+//            // Set a default budget value if the budgetTextField is empty or invalid
+//            newRequestToBeCreated = Request(category: self.categoryName, service: self.serviceName, budget: 0.0, description: self.descriptionTextView.text, isUrgent: true, createdDate: formattedDate, status: "Not Done")
+//            budgetTextField.text = "$0.00" // Set the initial text with the $0.00 mask
+//        }
         
         // for tapping
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
     }
-    
+
+	func validateBudgetInput() {
+		guard var budgetValue = budgetTextField.text else {
+			return
+		}
+		let valueString = budgetValue.filter {
+			CharacterSet(charactersIn: "0123456789.").isSuperset(of: CharacterSet(charactersIn: String($0)))
+		}.trimmingCharacters(in: .whitespacesAndNewlines)
+		print("---> valueString: \(valueString) ")
+		// change the hardcoded dollar sign into the locale currency
+		// filter any periods after the first one
+		budgetTextField.text = "$ " + valueString
+	}
+
     // Handle tapping to dismiss keyboard
     @objc func dismissKeyboard() {
         view.endEditing(true)
@@ -57,7 +72,10 @@ class ServiceRequestViewController: UIViewController, UITextViewDelegate, UIText
             // The budget will be formatted just before submission in the submitButton function
         }
     }
-    
+	@IBAction func budgetValueChanged(_ sender: Any) {
+		self.validateBudgetInput()
+	}
+	
     
     @IBAction func switchDidChange(_ sender: UISwitch) {
         // Update the isUrgent property of the newRequestToBeCreated object
@@ -110,5 +128,69 @@ extension ServiceRequestViewController /*: UITextFieldDelegate*/ {
     }
 }
 
+// please try to understand before you use it
 
+class CurrencyTextField: UITextField {
+
+	/// The numbers that have been entered in the text field
+	private var enteredNumbers = ""
+
+	private var didBackspace = false
+
+	var locale: Locale = .current
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		commonInit()
+	}
+
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		commonInit()
+	}
+
+	private func commonInit() {
+		addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+	}
+
+	override func deleteBackward() {
+		enteredNumbers = String(enteredNumbers.dropLast())
+		text = enteredNumbers.asCurrency(locale: locale)
+		// Call super so that the .editingChanged event gets fired, but we need to handle it differently, so we set the `didBackspace` flag first
+		didBackspace = true
+		super.deleteBackward()
+	}
+
+	@objc func editingChanged() {
+		defer {
+			didBackspace = false
+			text = enteredNumbers.asCurrency(locale: locale)
+		}
+
+		guard didBackspace == false else { return }
+
+		if let lastEnteredCharacter = text?.last, lastEnteredCharacter.isNumber {
+			enteredNumbers.append(lastEnteredCharacter)
+		}
+	}
+}
+
+private extension Formatter {
+	static let currency: NumberFormatter = {
+		let formatter = NumberFormatter()
+		formatter.numberStyle = .currency
+		return formatter
+	}()
+}
+
+private extension String {
+	func asCurrency(locale: Locale) -> String? {
+		Formatter.currency.locale = locale
+		if self.isEmpty {
+			return Formatter.currency.string(from: NSNumber(value: 0))
+		} else {
+			return Formatter.currency.string(from: NSNumber(value: (Double(self) ?? 0) / 100))
+		}
+	}
+}
 
